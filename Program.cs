34e2cf7332bb43513b4a82fd6384a45afd3e1435
AliceHh0 <50793870+AliceHh0@users.noreply.github.com>
2024-07-17 12:09:52 +0800

    This is my first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * Working Directory > Staging Area > Github Repo
+ * git init > git add > git commit
+ * 
+ * 
+ * 
+ * #################### List of Commands ###########################
+ * git init
+ * git add
+ * git status
+ * git diff : to check difference in version (+ is the newly added changes)
+ * git commit -m : -m to add messages for commit action
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Steps:
+ * 1. git init
+ * 2. create a gitignore file to ignore certain visual studio file
+ * 3. git add. to add all files (it will ignore files listed in git ignore)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
