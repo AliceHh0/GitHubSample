8f14b78bec075c6169f787e1b4813407921e2b5c
AliceHh0 <50793870+AliceHh0@users.noreply.github.com>
2024-07-23 08:46:29 +0800

    The first final version of commit, with proper setup of gitignore

--- Program.cs
+++ Program.cs
@@ -1,27 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
 /*
- * Working Directory > Staging Area > Github Repo
+ * Working Directory > Staging Area(will be tracked) > Github Repo
  * git init > git add > git commit
  * 
  * 
  * 
  * #################### List of Commands ###########################
  * git init
- * git add
+ * git add -n : -n not to be add but will show if file is exist or ignored
+ *         -f : stage all including ignore
+ *         .  : stage all except ignored
+ *         -A : stage all including subdirectories
+ *         filename1 filename2 : stage specific files
+ *         
  * git status
  * git diff : to check difference in version (+ is the newly added changes)
- * git commit -m : -m to add messages for commit action
+ * git commit -m "text" : -m to add messages for commit action
+ *            --amend (--no-edit > keep same commit msg)   : commit this and replace the most recent commit ( to keep less commit, and more clean!!)
+ * git log : return commit history with a unique identifier called SHA
+ *         --oneline : to show in one line
+ *         -S keyword(Add/Deletion/Modification) : to filter based on keyword , ex: only related to addition
+ *         -- graph - visual representation, can be combine with oneline for simplification
+ * git show HEAD : to see the commit at HEAD (the commit that you are currently on)
+ * git checkout HEAD filename : update HEAD pointer to another branch, at the same time delete whatever file u working on at working directory, and instead show the file in new pointer
+ *                              need to close file, and open back file to see the changes
+ * git reset HEAD filename : pointer points same branch, but the commit is discarded, 
+ *           commitSHAnumber : remove file from staging area and back to this specific commit
+ * git stash : temporarily stash work at working directory, and then use {git stash pop} to retrieve back, can store multiple
+ * git stash list : to see git stash
  * 
  * 
  * 
+ * #################### Useful Tips ###########################
+ * Setting Up Alias [To shorten, shortcut key commands]
+ * $ git config --global alias.co "checkout"
+ * $ git co filename vs git checkout filename
  * 
+ * ctrl+shift+insert to paste in Git Bash
  * 
  * Steps:
- * 1. git init
- * 2. create a gitignore file to ignore certain visual studio file
- * 3. git add. to add all files (it will ignore files listed in git ignore)
+ * 1. mkdir git_main_file > make a new directory
+ * 2. go to the newly created directory
+ * 3. git init
+ * 4. create a gitignore file to ignore certain visual studio file
+ * 5. git add. to add all files (it will ignore files listed in git ignore)
+ * 6. git commit -m "Commit message"
+ * 7. Connect local repo to online Github repo (make sure working tree clean, no staging files)
+ * 8. git remote add origin https://github.com/AliceHh0/git_practice.git to add local repo to the online repo
+ * 9. git branch -M main
+ * 10. git push -u origin main
  * 
  * 
  * 
